61ac9b6de7dc511c0946369ad87de99bc4125bfa
Antonis Ioannou <antonios.ioannou@protonmail.com>
2022-05-06 16:54:31 +0300

    spinner

--- BlazorWebAssemblyTutorial/Server/Models/AppDbContext.cs
+++ BlazorWebAssemblyTutorial/Server/Models/AppDbContext.cs
@@ -18,68 +18,70 @@ namespace BlazorWebAssemblyTutorial.Server.Models
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Department> Departments  { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        #region seeding 
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
 
-            //Seed Departments Table
-            modelBuilder.Entity<Department>().HasData(
-                new Department { DepartmentId = 1, DepartmentName = "IT" });
-            modelBuilder.Entity<Department>().HasData(
-                new Department { DepartmentId = 2, DepartmentName = "HR" });
-            modelBuilder.Entity<Department>().HasData(
-                new Department { DepartmentId = 3, DepartmentName = "Payroll" });
-            modelBuilder.Entity<Department>().HasData(
-                new Department { DepartmentId = 4, DepartmentName = "Admin" });
+        //    //Seed Departments Table
+        //    modelBuilder.Entity<Department>().HasData(
+        //        new Department { DepartmentId = 1, DepartmentName = "IT" });
+        //    modelBuilder.Entity<Department>().HasData(
+        //        new Department { DepartmentId = 2, DepartmentName = "HR" });
+        //    modelBuilder.Entity<Department>().HasData(
+        //        new Department { DepartmentId = 3, DepartmentName = "Payroll" });
+        //    modelBuilder.Entity<Department>().HasData(
+        //        new Department { DepartmentId = 4, DepartmentName = "Admin" });
 
-            //Seed Employee Table
-            modelBuilder.Entity<Employee>().HasData(new Employee
-            {
-                EmployeeId = 1,
-                FirstName = "Bruce",
-                LastName = "Jones",
-                Email = "bruce@alphaSoftware.com",
-                DateOfBirth = new DateTime(1980, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/bruce.png"
-            });
+        //    //Seed Employee Table
+        //    modelBuilder.Entity<Employee>().HasData(new Employee
+        //    {
+        //        EmployeeId = 1,
+        //        FirstName = "Bruce",
+        //        LastName = "Jones",
+        //        Email = "bruce@alphaSoftware.com",
+        //        DateOfBirth = new DateTime(1980, 10, 5),
+        //        Gender = Gender.Male,
+        //        DepartmentId = 1,
+        //        PhotoPath = "images/bruce.png"
+        //    });
 
-            modelBuilder.Entity<Employee>().HasData(new Employee
-            {
-                EmployeeId = 2,
-                FirstName = "Julie",
-                LastName = "Adams",
-                Email = "julie@alphaSoftware.com",
-                DateOfBirth = new DateTime(1984, 11, 11),
-                Gender = Gender.Female,
-                DepartmentId = 2,
-                PhotoPath = "images/julie.png"
-            });
+        //    modelBuilder.Entity<Employee>().HasData(new Employee
+        //    {
+        //        EmployeeId = 2,
+        //        FirstName = "Julie",
+        //        LastName = "Adams",
+        //        Email = "julie@alphaSoftware.com",
+        //        DateOfBirth = new DateTime(1984, 11, 11),
+        //        Gender = Gender.Female,
+        //        DepartmentId = 2,
+        //        PhotoPath = "images/julie.png"
+        //    });
 
-            modelBuilder.Entity<Employee>().HasData(new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Ian",
-                LastName = "Claff",
-                Email = "ian@alphaSoftware.com",
-                DateOfBirth = new DateTime(1983, 12, 4),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/ian.png"
-            });
+        //    modelBuilder.Entity<Employee>().HasData(new Employee
+        //    {
+        //        EmployeeId = 3,
+        //        FirstName = "Ian",
+        //        LastName = "Claff",
+        //        Email = "ian@alphaSoftware.com",
+        //        DateOfBirth = new DateTime(1983, 12, 4),
+        //        Gender = Gender.Male,
+        //        DepartmentId = 1,
+        //        PhotoPath = "images/ian.png"
+        //    });
 
-            modelBuilder.Entity<Employee>().HasData(new Employee
-            {
-                EmployeeId = 4,
-                FirstName = "Loreena",
-                LastName = "Rush",
-                Email = "loreena@alphaSoftware.com",
-                DateOfBirth = new DateTime(1981, 09, 12),
-                Gender = Gender.Female,
-                DepartmentId = 3,
-                PhotoPath = "images/loreena.png"
-            });
-        }
+        //    modelBuilder.Entity<Employee>().HasData(new Employee
+        //    {
+        //        EmployeeId = 4,
+        //        FirstName = "Loreena",
+        //        LastName = "Rush",
+        //        Email = "loreena@alphaSoftware.com",
+        //        DateOfBirth = new DateTime(1981, 09, 12),
+        //        Gender = Gender.Female,
+        //        DepartmentId = 3,
+        //        PhotoPath = "images/loreena.png"
+        //    });
+        //}
+        #endregion
     }
 }
